e7aff6dbb6e0d05f6bd6316aa947ac67c85b5394
Fernando Dodino <fernando.dodino@gmail.com>
2023-09-05 19:57:44 -0300

    2023 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,62 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion: String by project
-
 plugins {
-    id("org.springframework.boot") version "2.7.4"
-    id("io.spring.dependency-management") version "1.0.14.RELEASE"
-    war
-    kotlin("jvm") version("1.7.10")
-    kotlin("plugin.spring") version("1.7.10")
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.1"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+    kotlin("plugin.jpa") version "1.9.0"
     jacoco
+    war
 }
+
+extra["springCloudVersion"] = "2022.0.4"
 
 group = "ar.edu.unsam"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
-val springBootVersion = "2.7.4"
-val kotestVersion = "5.4.2"
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    val kotestVersion = "5.4.2"
+
+    // básicos de cualquier proyecto Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
-    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat:$springBootVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2") //
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.14") //
+    implementation("org.springframework.boot:spring-boot-devtools")
+
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+    // testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.12.8")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "14"
+        jvmTarget = "17"
     }
 }
 
@@ -54,7 +73,7 @@ tasks.jacocoTestReport {
 }
 
 jacoco {
-    toolVersion = "0.8.7"
+    toolVersion = "0.8.10"
 }
 
 tasks.jacocoTestReport {
